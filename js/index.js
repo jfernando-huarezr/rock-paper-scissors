ee7ddae1f4f5b3228a8b9a3a664dcab099ac3e3f
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-09-26 17:11:24 -0500

    english translation

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
-const seleccion = ["Piedra", "Papel", "Tijera"]
+const selection = ["Rock", "Paper", "Scissors"]
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
 getComputerChoice = function() {
-    const jugada = getRandomInt(3)
-    return jugada
+    const move = getRandomInt(3)
+    return move
 }
 
 playerSelection = function() {
   while(true) {
-    let jugada = window.prompt("Escribe piedra, papel o tijera")
-    jugada = jugada.toLowerCase()
-    jugada = jugada.split("")
-    jugada[0] = jugada[0].toUpperCase()
-    jugada = jugada.join("")
+    let move = window.prompt("Write Rock, Paper or Scissors")
+    move = move.toLowerCase()
+    move = move.split("")
+    move[0] = move[0].toUpperCase()
+    move = move.join("")
 
-    for(i=0;i<seleccion.length;i++) {
-        if (seleccion[i] === jugada) {
+    for(i=0;i<selection.length;i++) {
+        if (selection[i] === move) {
             return i
         }                   
     }
 
-    window.alert("no ha ingresado una jugada valida")
+    window.alert("That's not a valid move")
   }
 }
 
@@ -35,47 +35,62 @@ playRound = function() {
     console.log(player)
 
     if (cpu === player) {
-        console.log(`Empate, ambos sacaron ${seleccion[player]}`)
+        console.log(`Draw, both got ${selection[player]}`)
         return 0
     }
 
     if (player-cpu === 1 || player-cpu === -2) {
-        console.log(`Ganaste, ${seleccion[player]} le gana a ${seleccion[cpu]}`)
+        console.log(`You win, ${selection[player]} beats ${selection[cpu]}`)
         return 1
     }
 
     if (player-cpu === -1 || player-cpu === 2) {
-        console.log(`Perdiste, ${seleccion[cpu]} le gana a ${seleccion[player]}`)
+        console.log(`You lose, ${selection[cpu]} beats ${selection[player]}`)
         return -1
     }
 }
 
 game = function() {
-    let totalJugador = 0
+    let totalPlayer = 0
     let totalCPU = 0
-    let empate =0
+    let draw =0
 
     for(let i=0; i<5; i++){
-        let resultado = playRound()
-        console.log(`estoy en el loop ${i}`)
-        switch (resultado) {
+        let result = playRound()
+        console.log(`I'm in the loop ${i}`)
+        switch (result) {
             case 1: 
-                totalJugador++;
+                totalPlayer++;
                 break;
             case -1: 
                 totalCPU++
                 break;
             case 0: 
-                empate++;
+                draw++;
                 break;
 
         }
     }
 
-    console.log(`El resultado final es:
-                JUGADOR: ${totalJugador}
+    let message = ""
+    if (totalPlayer > totalCPU){
+        message = "YOU ARE THE WINNER"
+    }
+
+    else if (totalCPU > totalPlayer) {
+        message = "YOU ARE THE LOSER"
+    }
+
+    else {
+        message = "WE HAVE A DRAW"
+    }
+
+    console.log(`The final score is:
+                Player: ${totalPlayer}
                 CPU: ${totalCPU}
-                Empates: ${empate}`)
+                Draws: ${draw}
+                
+                ${message}`)
 }
 
 
